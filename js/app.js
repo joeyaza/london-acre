ba4536facbbb69ba1260392396bccf4bd054614f
Joseph Azadehdel <Joes@Josephs-Air.home>
2016-04-11 22:29:25 +0100

    blurriness gone

--- js/app.js
+++ js/app.js
@@ -2,6 +2,6 @@ $(function(){
   $('ul.links a').on('click', function(){
     var ref = $(this).attr('data-id');
     console.log("clicked", ref)
-    $('body').scrollTo($('#'+ref), 1000)
+    $('body').scrollTo($('#'+ref), 2000)
   })
 })
