0bcec03c079ee7e8dedb358422768eed600376c2
Joseph Azadehdel <Joes@Josephs-Air.home>
2016-04-19 21:21:49 +0100

    blurriness fixed in chrome

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(function(){
-  $('ul.links a').on('click', function(){
+  $('a').on('click', function(){
     var ref = $(this).attr('data-id');
     console.log("clicked", ref)
     $('body').scrollTo($('#'+ref), 2000)
