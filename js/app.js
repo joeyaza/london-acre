cffca05a3033a1e7d9b56e4abe21cd1c988dc806
Joseph Azadehdel <Joes@Josephs-Air.home>
2016-08-07 22:36:29 +0100

    update to maps and styling to fix fuzziness

--- js/app.js
+++ js/app.js
@@ -6,3 +6,18 @@ jQuery(function(){
   })
   event.preventDefault();
 });
+
+function initMap() {
+
+      var myLatLng = {lat: 51.54155, lng:  -0.145719};
+      // Create a map object and specify the DOM element for display.
+      var map = new google.maps.Map(document.getElementById('map'), {
+        scrollwheel: false,
+        zoom: 15,
+        center: myLatLng
+      });
+      var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map
+      });
+    }
